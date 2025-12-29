099c222b4cfc1d11521632afd37738c47f7a499d
mcMineyC <jedi.master@falconhosting.net>
2025-12-28 17:50:34 -0800

    Actually fix weird control character error :shrug:

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,21 +1,28 @@
 import '../views/components/generics.dart';
 
 String kVersionString = "0.0.7.8";
-String kDashboardConfig = '''[
+String kDashboardConfig = r'''[
+  {
+    "label": "Spotify",
+    "integrationId": "spotify",
+    "type": "mediaPlayer",
+    "evaluatorScript": "",
+    "outputTransformer": ""
+  },
   [
     {
       "label": "Bedside Bulb",
       "integrationId": "bed-bulb",
       "type": "button",
       "evaluatorScript": "      var lightState = props['/lightState'];\n      var value = lightState['on_off'] == 1;\n      return {\n        \"text\": value ? \"Turn Off\" : \"Turn On\",\n        \"icon\": value ? \"lightbulb\" : \"lightbulb_outlined\",\n        \"value\": value,\n      };\n    ",
-      "outputTransformer": "      return {\n        \"path\": \"/\${integrationId}/power/toggle\",\n        \"data\": \"true\",\n      };\n    "
+      "outputTransformer": "      return {\n        \"path\": \"/${integrationId}/power/toggle\",\n        \"data\": \"true\",\n      };\n    "
     },
     {
       "label": "Desk Bulb",
       "integrationId": "desk-bulb",
       "type": "button",
       "evaluatorScript": "      var lightState = props['/lightState'];\n      var value = lightState['on_off'] == 1;\n      return {\n        \"text\": value ? \"Turn Off\" : \"Turn On\",\n        \"icon\": value ? \"lightbulb\" : \"lightbulb_outlined\",\n        \"value\": value,\n      };\n    ",
-      "outputTransformer": "      return {\n        \"path\": \"/\${integrationId}/power/toggle\",\n        \"data\": \"true\",\n      };\n    "
+      "outputTransformer": "      return {\n        \"path\": \"/${integrationId}/power/toggle\",\n        \"data\": \"true\",\n      };\n    "
     }
   ],
   [
@@ -24,14 +31,14 @@ String kDashboardConfig = '''[
       "integrationId": "bed-bulb",
       "type": "slider",
       "evaluatorScript": "      var lightState = props['/lightState'];\n      var temperatureRange = props['/temperatureRange'];\n      return {\n        \"value\": lightState['color_temp'],\n        \"min\": temperatureRange['min'],\n        \"max\": temperatureRange['max'],\n      };\n    ",
-      "outputTransformer": "      return {\n        \"path\": \"/\${integrationId}/light/temperature\",\n        \"data\": props[\"value\"].toString(),\n      };\n    "
+      "outputTransformer": "      return {\n        \"path\": \"/${integrationId}/light/temperature\",\n        \"data\": props[\"value\"].toString(),\n      };\n    "
     },
     {
       "label": "Bed Brightness",
       "integrationId": "bed-bulb",
       "type": "slider",
       "evaluatorScript": "      var lightState = props['/lightState'];\n      return {\n        \"value\": lightState['brightness'],\n        \"min\": 0,\n        \"max\": 100,\n      };\n    ",
-      "outputTransformer": "      return {\n        \"path\": \"/\${integrationId}/light/brightness\",\n        \"data\": props[\"value\"].toString(),\n      };\n    "
+      "outputTransformer": "      return {\n        \"path\": \"/${integrationId}/light/brightness\",\n        \"data\": props[\"value\"].toString(),\n      };\n    "
     }
   ],
   [
@@ -40,14 +47,14 @@ String kDashboardConfig = '''[
       "integrationId": "desk-bulb",
       "type": "slider",
       "evaluatorScript": "      var lightState = props['/lightState'];\n      var temperatureRange = props['/temperatureRange'];\n      return {\n        \"value\": lightState['color_temp'],\n        \"min\": temperatureRange['min'],\n        \"max\": temperatureRange['max'],\n      };\n    ",
-      "outputTransformer": "      return {\n        \"path\": \"/\${integrationId}/light/temperature\",\n        \"data\": props[\"value\"].toString(),\n      };\n    "
+      "outputTransformer": "      return {\n        \"path\": \"/${integrationId}/light/temperature\",\n        \"data\": props[\"value\"].toString(),\n      };\n    "
     },
     {
       "label": "Desk Brightness",
       "integrationId": "desk-bulb",
       "type": "slider",
       "evaluatorScript": "      var lightState = props['/lightState'];\n      return {\n        \"value\": lightState['brightness'],\n        \"min\": 0,\n        \"max\": 100,\n      };\n    ",
-      "outputTransformer": "      return {\n        \"path\": \"/\${integrationId}/light/brightness\",\n        \"data\": props[\"value\"].toString(),\n      };\n    "
+      "outputTransformer": "      return {\n        \"path\": \"/${integrationId}/light/brightness\",\n        \"data\": props[\"value\"].toString(),\n      };\n    "
     }
   ],
   {
@@ -55,13 +62,13 @@ String kDashboardConfig = '''[
     "integrationId": "broom-closet-ending",
     "type": "button",
     "evaluatorScript": "      var lightState = props['/lightState'];\n      var value = lightState['on_off'] == 1;\n      return {\n        \"text\": value ? \"Turn Off\" : \"Turn On\",\n        \"icon\": value ? \"lightbulb\" : \"lightbulb_outlined\",\n        \"value\": value,\n      };\n    ",
-    "outputTransformer": "      return {\n        \"path\": \"/\${integrationId}/power/toggle\",\n        \"data\": \"true\",\n      };\n    "
+    "outputTransformer": "      return {\n        \"path\": \"/${integrationId}/power/toggle\",\n        \"data\": \"true\",\n      };\n    "
   },
   {
     "label": "Poweroff",
     "integrationId": "poweroffer",
     "type": "statelessButton",
     "evaluatorScript": "     return {\n        \"text\": \"Shutdown\",\n        \"icon\": \"power_settings_new_rounded\",\n      };\n    ",
-    "outputTransformer": "      return {\n        \"path\": \"/\${integrationId}/poweroff\",\n        \"data\": \"true\",\n      };\n    "
+    "outputTransformer": "      return {\n        \"path\": \"/${integrationId}/poweroff\",\n        \"data\": \"true\",\n      };\n    "
   }
 ]''';
